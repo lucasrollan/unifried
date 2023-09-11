b53eb3db3643624a1624cea4d08e870162099a94
Lucas Rollan <lucas.ro@miro.com>
2023-09-11 15:40:51 +0200

    chore: Set up Google authentication

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
+}
+
+export default App;
+
