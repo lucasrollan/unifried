ea4f2298eae2f61668bf3f03aa950e5b39fb81ae
Lucas Rollan <lucas.ro@miro.com>
2023-09-18 13:34:35 +0200

    chore: import layout from relative path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-import Layout from "@/components/layout";
+import Layout from "../components/layout";
 import { store } from '@/store'
 import { Provider as StateProvider } from 'react-redux'
 
