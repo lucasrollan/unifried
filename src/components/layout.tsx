7098579261a48f809144a50cbdcd16ee7d9aef9c
Lucas Rollan <lucas.ro@miro.com>
2023-09-18 13:32:23 +0200

    chore: Restored NavBar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,9 @@
 import '../main.css'
+import NavBar from "./NavBar";
 
 export default function Layout(props: any) {
     return <div className="layout">
+        <NavBar />
         <div className="fullHeight">{
             props.children
         }</div>
