4096a2b5675ffe307ceb773e9aec3408f77ac968
Lucas Rollan <lucas.ro@miro.com>
2023-09-18 13:29:22 +0200

    chore: Temporarily remove NavBar to debug Vercel build

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,7 @@
-import Session from "./session";
 import '../main.css'
-import NavBar from "./NavBar";
 
 export default function Layout(props: any) {
     return <div className="layout">
-        <NavBar />
         <div className="fullHeight">{
             props.children
         }</div>
