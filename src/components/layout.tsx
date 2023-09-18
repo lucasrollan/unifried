174f1d0482b2337d2ac9afb4a76e4c3657ceeabd
Lucas Rollan <lucas.ro@miro.com>
2023-09-18 13:22:28 +0200

    chore: Change NavBar import to relative path

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import Session from "./session";
 import '../main.css'
-import NavBar from "@/components/NavBar";
+import NavBar from "./NavBar";
 
 export default function Layout(props: any) {
     return <div className="layout">
