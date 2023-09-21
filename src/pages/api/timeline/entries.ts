71b260dab28e646957c4524826cddad794180f0a
Lucas Rollan <lucas.ro@miro.com>
2023-09-21 16:48:18 +0200

    feat: Added authentication to API endpoints

--- src/pages/api/timeline/entries.ts
+++ src/pages/api/timeline/entries.ts
@@ -1,18 +1,26 @@
 import { TimelineEntry } from '@/types/timeline'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getToken } from 'next-auth/jwt'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<TimelineEntry[]>
 ) {
-  res.status(200).json([
-    {
+  const token = await getToken({ req })
+  if (token) {
+    // Signed in
+    res.status(200).json([
+      {
         id: 'TST0000001',
         label: 'Test 001',
         start: '2023-12-05',
         end: '2023-12-06',
         isHighlighted: true,
-    }
-  ])
+      }
+    ])
+  } else {
+    // Not Signed in
+    res.status(401).json([])
+  }
 }
 
--- src/pages/api/timeline/rows.ts
+++ src/pages/api/timeline/rows.ts
@@ -1,15 +1,23 @@
 import { TimelineRow } from '@/types/timeline'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getToken } from 'next-auth/jwt'
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<TimelineRow[]>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<TimelineRow[]>
 ) {
-  res.status(200).json([
-    {
-        id: 'ROW_TEST',
-        label: 'Test',
-        entryIds: ['TST0000001'],
+    const token = await getToken({ req })
+    if (token) {
+        // Signed in
+        res.status(200).json([
+            {
+                id: 'ROW_TEST',
+                label: 'Test',
+                entryIds: ['TST0000001'],
+            }
+        ])
+    } else {
+        // Not Signed in
+        res.status(401).json([])
     }
-  ])
 }
