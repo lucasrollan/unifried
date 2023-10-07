d666ab77d87ece666edeaa61b7aa0c9a68145cca
Lucas Rollan <lucas.ro@miro.com>
2023-10-07 17:47:35 +0200

    feat: Start burbuja subdomain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    rewrites() {
+        return {
+            beforeFiles: [
+                // if the host is `app.acme.com`,
+                // this rewrite will be applied
+                {
+                    source: '/:path*',
+                    has: [
+                        {
+                            type: 'host',
+                            value: 'burbuja.rollan.com',
+                        },
+                    ],
+                    destination: '/burbuja/:path*',
+                },
+            ]
+        }
+    }
+}
 
 module.exports = nextConfig
---
+++ src/pages/burbuja/index.tsx
@@ -0,0 +1,5 @@
+export default function() {
+    return <div>
+        <h1>Bitacora de una Burbuja</h1>
+    </div>
+}
