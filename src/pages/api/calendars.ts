234cef6199499c95498d41b00662c47dd46fa820
Lucas Rollan <lucas.ro@miro.com>
2023-11-02 14:51:28 +0100

    refactor: Extract GoogleCalendarConnector

--- src/pages/api/calendars.ts
+++ src/pages/api/calendars.ts
@@ -1,69 +1,25 @@
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from "./auth/[...nextauth]"
-import { google } from 'googleapis'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
-import getGoogleAuth from './getGoogleAuth';
+import GoogleCalendarConnector from '@/persistence/GoogleCalendarConnector';
 import Calendar from '@/models/Calendar';
-import { GcalCalendar } from '@/models/gcal';
-
-const HIGHLIGHTED_CALENDARS_IDS = [
-    '4ef5086e1cd0b60a8fc47e3e530b1144244b93b6b119e71f455f46a7f647286a@group.calendar.google.com', // PTOs
-]
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<Calendar[]>
 ) {
     const session = await getServerSession(req, res, authOptions)
     if (session) {
-        const events = await fetchCalendarsFromGoogleCalendar()
+        const googleCalendar = GoogleCalendarConnector.getInstance()
+        const calendars = await googleCalendar.listCalendars()
 
         // Signed in
         res.status(200).json([
-            ...events,
+            ...calendars,
         ])
     } else {
         // Not Signed in
         res.status(401).json([])
     }
-}
-
-const googleAuth = getGoogleAuth()
-
-async function fetchCalendarsFromGoogleCalendar() {
-    const calendar = google.calendar({ version: 'v3', auth: googleAuth });
-
-    try {
-        const calendarList = await calendar.calendarList.list({
-            showDeleted: false,
-            showHidden: false,
-        })
-
-        const calendars: Calendar[] = await Promise.all(
-            calendarList.data.items?.map(async calendarListItem => {
-                const response = await calendar.calendars.get({
-                    calendarId: calendarListItem.id || 'primary',
-                })
-                return projectGcalCalendarToApp(response.data)
-            }) || []
-        )
-
-        return calendars
-    } catch (e: any) {
-        const err = e.response.data.error
-        console.error('Something bad happened', err, e)
-        return []
-    }
-}
-
-function projectGcalCalendarToApp (gcalCalendar: GcalCalendar): Calendar {
-    const isHighlighted = HIGHLIGHTED_CALENDARS_IDS.includes(gcalCalendar.id!)
-
-    return ({
-        id: gcalCalendar.id!,
-        isVisible: true,
-        label: gcalCalendar.summary!,
-        isHighlighted,
-    })
 }
---
+++ src/persistence/GoogleCalendarConnector.ts
@@ -0,0 +1,63 @@
+import { calendar_v3, google } from 'googleapis'
+import getGoogleAuth from "@/pages/api/getGoogleAuth";
+import Calendar from '@/models/Calendar';
+import { GcalCalendar } from '@/models/gcal';
+
+const HIGHLIGHTED_CALENDARS_IDS = [
+    '4ef5086e1cd0b60a8fc47e3e530b1144244b93b6b119e71f455f46a7f647286a@group.calendar.google.com', // PTOs
+]
+
+class GoogleCalendarConnector {
+    private static instance: GoogleCalendarConnector | null = null
+    private googleCalendar: calendar_v3.Calendar
+
+    private constructor() {
+        const googleAuth = getGoogleAuth()
+        this.googleCalendar = google.calendar({ version: 'v3', auth: googleAuth });
+    }
+
+    static getInstance(): GoogleCalendarConnector {
+        if (!GoogleCalendarConnector.instance) {
+            GoogleCalendarConnector.instance = new GoogleCalendarConnector()
+        }
+        return GoogleCalendarConnector.instance
+    }
+
+    async listCalendars() {
+        try {
+            const calendarList = await this.googleCalendar.calendarList.list({
+                showDeleted: false,
+                showHidden: false,
+            })
+
+            const calendars: Calendar[] = await Promise.all(
+                calendarList.data.items?.map(async calendarListItem => {
+                    const response = await this.googleCalendar.calendars.get({
+                        calendarId: calendarListItem.id || 'primary',
+                    })
+                    return projectGcalCalendarToApp(response.data)
+                }) || []
+            )
+
+            return calendars
+        } catch (e: any) {
+            const err = e.response.data.error
+            console.error('Something bad happened', err, e)
+            return []
+        }
+    }
+
+}
+
+function projectGcalCalendarToApp (gcalCalendar: GcalCalendar): Calendar {
+    const isHighlighted = HIGHLIGHTED_CALENDARS_IDS.includes(gcalCalendar.id!)
+
+    return ({
+        id: gcalCalendar.id!,
+        isVisible: true,
+        label: gcalCalendar.summary!,
+        isHighlighted,
+    })
+}
+
+export default GoogleCalendarConnector
