4c62ef9ec5c1348fc3039554e4193fe272f0f307
Lucas Rollan <lucas.ro@miro.com>
2023-11-02 10:48:48 +0100

    refactor: Create unused Fragment class

--- src/models/Fragment.ts
+++ src/models/Fragment.ts
@@ -1,27 +1,39 @@
-type dateString = string
-type dateTimeString = string
+import IFragment from "./IFragment"
 
-type Fragment = {
-    id: string,
-    title: string,
-    priority?: number,
-    role: string,
-    content?: string,
-    createdDate: dateString,
-    modifiedDate: dateString,
-    relatedTo?: string[],
-    parentId?: string,
-    status?: string,
-    earliestStart?: dateTimeString,
-    earliestStartDate?: dateString,
-    start?: dateTimeString,
-    startDate?: dateString,
-    end?: dateTimeString,
-    endDate?: dateString,
-    location?: string,
-    reward?: number,
-    isCompleted: boolean,
-    completionDate?: dateTimeString,
+class Fragment {
+    fragment: IFragment
+
+    constructor(fragment: IFragment) {
+        this.fragment = fragment
+    }
+
+    validateInvariants(): void {
+        if (this.fragment.role === 'task') {
+            if (this.fragment.status === 'completed' || this.fragment.completionDate || this.fragment.isCompleted) {
+                // if one of these is defined, the all have to be defined
+                if (this.fragment.status !== 'completed' || !this.fragment.completionDate || !this.fragment.isCompleted) {
+                    console.log('INVALID FRAGMENT')
+                    console.log(this.fragment)
+                    throw new Error('Task completed with invalid values')
+                }
+            }
+        }
+    }
+
+    update(fragment: IFragment, updates: Partial<IFragment>): void {
+        const updatedFragment = {
+            ...fragment,
+            ...updates,
+        }
+
+        this.validateInvariants()
+
+        this.fragment = updatedFragment
+    }
+
+    serialize(): string {
+        return JSON.stringify(this.fragment)
+    }
 }
 
 export default Fragment
