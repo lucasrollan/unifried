3718abfab2be9baec45511026cb3186f08a5a6cb
Lucas Rollan <lucas.ro@miro.com>
2024-01-23 13:45:52 +0100

    chore(day): Added title to daily tasks page

--- src/pages/day.tsx
+++ src/pages/day.tsx
@@ -1,10 +1,14 @@
 import SessionGuard from "@/components/SessionGuard";
 import DaySummaryPage from "@/components/daySummaryPage/Page";
+import Head from "next/head";
 import React from "react";
 
 export default function DaySummaryPage2() {
     return (
       <SessionGuard>
+        <Head>
+          <title>Daily tasks</title>
+        </Head>
         <main className="fullHeight">
             <DaySummaryPage />
         </main>
